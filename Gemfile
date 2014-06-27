646f4a49900f23ca03799d34b70083c71c3001a2
Jasonm23 <jasonm23@gmail.com>
2014-06-27 17:28:30 +0800

    Converted to rubygem
    - added gemspec
    - command line options added with Slop
    - deftheme output via erubis template
    - added color palette interpretation to generate rainbow delimiters
    - RGBA convert to tinted solid colors
    - add error checking for input file tmTheme

--- Gemfile
+++ Gemfile
@@ -2,3 +2,8 @@ source 'https://rubygems.org'
 
 gem 'plist4r'
 gem 'color'
+gem 'slop'
+gem 'erubis'
+
+gem 'pry'
+
---
+++ spec/spec_helper.rb
@@ -0,0 +1,8 @@
+require 'bundler/setup'
+Bundler.setup
+
+require 'tmtheme-to-deftheme' # and any other gems you need
+
+RSpec.configure do |config|
+  # some (optional) config here
+end
