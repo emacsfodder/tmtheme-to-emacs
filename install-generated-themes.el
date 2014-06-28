2083879921d41920fba4d03687dc15dde12f9f99
Jasonm23 <jasonm23@gmail.com>
2014-06-29 05:09:05 +0800

    Fix typo in install-generated-themes.el - note to self, wear your glasses.

--- install-generated-themes.el
+++ install-generated-themes.el
@@ -1,6 +1,6 @@
 ;; Dirt simple loop loading of generated themes
 (mapc
  #'(lambda (f)
-     (message "Installing,,, %s" f)
+     (message "Installing... %s" f)
      (package-install-file f))
  (directory-files "./generatedThemes/" t ".*-theme\\.el" ))
