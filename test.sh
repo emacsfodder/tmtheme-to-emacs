534c5c83e916280d2595ecc7ca8e5193fdb8cd67
Jasonm23 <jasonm23@gmail.com>
2014-06-28 17:28:08 +0800

    Bump to 0.1.7

--- test.sh
+++ test.sh
@@ -2,8 +2,8 @@
 
 ls -d ~/.emacs.d/elpa/*-theme-140* -1 | xargs rm -rf
 cd ~/workspace/tmtheme-to-deftheme
-gem build -V tm2deftheme.gemspec && gem install -V -l tm2deftheme-0.1.6.gem
-rm tm2deftheme-0.1.6.gem
+gem build -V tm2deftheme.gemspec && gem install -V -l tm2deftheme-0.1.7.gem
+rm tm2deftheme-0.1.7.gem
 cd ~/workspace/tmtheme-to-deftheme/generatedThemes
 for a in ../sampleThemes/*.tmTheme
 do
