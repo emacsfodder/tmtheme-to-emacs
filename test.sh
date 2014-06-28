b44131fd5ef47d976881d0112ba7dd95e10f1a1b
Jasonm23 <jasonm23@gmail.com>
2014-06-28 15:19:34 +0800

    bump test script

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 ls -d ~/.emacs.d/elpa/*-theme-140* -1 | xargs rm -rf
 cd ~/workspace/tmtheme-to-deftheme
-gem build -V tm2deftheme.gemspec && gem install -V -l tm2deftheme-0.1.5.gem
+gem build -V tm2deftheme.gemspec && gem install -V -l tm2deftheme-0.1.6.gem
 cd ~/workspace/tmtheme-to-deftheme/generatedThemes
 for a in ../sampleThemes/*.tmTheme
 do
